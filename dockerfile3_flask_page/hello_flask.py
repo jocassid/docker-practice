379e5eb23e2e82655fb27a140b7e0d760f0f8216
John Cassidy <jocassid@gmail.com>
2026-01-05 23:40:13 -0500

    Did Pipfile get deleted?

--- dockerfile3_flask_page/hello_flask.py
+++ dockerfile3_flask_page/hello_flask.py
@@ -1,7 +1,11 @@
+from datetime import datetime
+
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
-    return '<p>Hello, World</p>'
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M')
+    return f'<p>Hello, World.  The current datetime is {current_time}</p><p>Another paragraph</p>'
